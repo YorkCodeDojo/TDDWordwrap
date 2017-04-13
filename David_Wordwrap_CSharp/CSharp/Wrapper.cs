0ce0cfc97d428dc3ec3c3b49626e072a95a1f0a5
DavidBetteridge <davidabetteridge@gmail.com>
2017-04-13 08:50:24 +0100

    My attempt

---
+++ David_Wordwrap_CSharp/CSharp/Wrapper.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp
+{
+    public class Wrapper
+    {
+        public string WrapText(string theText, int columnWidth)
+        {
+            if (theText.Length > columnWidth)
+            {
+                var finalSpace = theText.Substring(0, columnWidth).LastIndexOf(' ');
+                var hypen = theText.Substring(0, columnWidth).LastIndexOf('-');
+                if (finalSpace > 0 && finalSpace > hypen)
+                    return theText.Substring(0, finalSpace + 1) + '\n' + WrapText(theText.Substring(finalSpace + 1), columnWidth);
+                else if (hypen > 0)
+                    return theText.Substring(0, hypen) + '\n' + WrapText(theText.Substring(hypen), columnWidth);
+                else
+                    return theText.Substring(0, columnWidth) + '\n' + WrapText(theText.Substring(columnWidth), columnWidth);
+            }
+            else
+                return theText;
+        }
+    }
+}
---
+++ David_Wordwrap_CSharp/David.Tests/WrapperTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp;
+
+namespace David.Tests
+{
+    [TestClass]
+    public class WrapperTests
+    {
+        [TestMethod]
+        public void AnEmptyStringReturnsAnEmptyString()
+        {
+            var wrapper = new Wrapper();
+            var theText = "";
+            var columnWidth = 10;
+            var expectedResult = "";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void AStringShorterThanTheColumnWidthIsReturnedUnaltered()
+        {
+            var wrapper = new Wrapper();
+            var theText = "Hello";
+            var columnWidth = 30;
+            var expectedResult = "Hello";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ASingleLongWordIsChoppedAtTheColumnWidth()
+        {
+            var wrapper = new Wrapper();
+            var theText = "Hello";
+            var columnWidth = 3;
+            var expectedResult = "Hel\nlo";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void ASingleVeryLongWordIsChoppedAtTheColumnWidths()
+        {
+            var wrapper = new Wrapper();
+            var theText = "HelloWorld";
+            var columnWidth = 3;
+            var expectedResult = "Hel\nloW\norl\nd";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TwoWordsAreChoppedOnTheSpaceRatherThanOnTheColumnWidth()
+        {
+            var wrapper = new Wrapper();
+            var theText = "Hello World";
+            var columnWidth = 7;
+            var expectedResult = "Hello \nWorld";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ThreeWordsAreChoppedOnTheSpacesRatherThanOnTheColumnWidth()
+        {
+            var wrapper = new Wrapper();
+            var theText = "York Code Dojo";
+            var columnWidth = 6;
+            var expectedResult = "York \nCode \nDojo";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ASingleLongWordIsChoppedAtAHyphen()
+        {
+            var wrapper = new Wrapper();
+            var theText = "York-Code-Dojo";
+            var columnWidth = 10;
+            var expectedResult = "York-Code\n-Dojo";
+
+            var actualResult = wrapper.WrapText(theText, columnWidth);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
